bcab9350def3073d5ebd213552cb498c2938b722
Eslam Ahmed <eslama051@gmail.com>
2023-02-16 08:38:44 +0200

    edits

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,14 @@
+const Task = require("../models/Task");
+
 module.exports.getTasks = (req, res) => {
   res.status(200).send("hello from tasks controllers");
 };
 module.exports.getTask = (req, res) => {
   res.status(200).send("get one task");
 };
-module.exports.createTask = (req, res) => {
-  res.status(200).send("create a new task ");
+module.exports.createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(200).json({ task });
 };
 module.exports.updateTask = (req, res) => {
   res.status(200).send("update one task");
